5f1e2cff683d8e174ef67f951f37b9588ef10928
Rudy Matela <rudy@matela.com.br>
2018-11-28 20:04:11 -0300

    improve eg/test-text

--- eg/test-text.hs
+++ eg/test-text.hs
@@ -4,8 +4,28 @@
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Test.LeanCheck
 import Test.LeanCheck.Instances
-import Data.Text
+import Data.Char
+import Data.Text as T
+import Prelude as P hiding (lines, unlines, words, unwords)
 
 main :: IO ()
 main = do
+  -- wrong: append is clearly not commutative
   check $ \t1 t2 -> t1 `append` t2 == t2 `append` t1
+
+  -- wrong: lines and unlines do not form an identity
+  check $ \ts -> lines (unlines ts) == ts
+  check $ \t ->   unlines (lines t) == t
+
+  -- correct: lines and unlines form an identity under certain conditions
+  check $ \ts -> P.all (T.all isAlphaNum) ts
+             ==> lines (unlines ts) == ts
+  check $ \ts -> unlines (lines (unlines ts)) == unlines ts
+  check $ \ts ->   lines (unlines (lines ts)) == lines ts
+
+  -- wrong: words and unwords do not form an identity
+  check $ \ts -> words (unwords ts) == ts
+  check $ \t ->   unwords (words t) == t
+
+  -- correct: words and unwords form an identity under certain conditions
+  check $ \ts ->   words (unwords (words ts)) == words ts
