8a59628167b4356b5e15bca8b253d053ff6cc1a1
Rudy Matela <rudy@matela.com.br>
2018-12-08 16:59:46 -0300

    Data.Set.Lazy does not exist!

--- mk/depend.mk
+++ mk/depend.mk
@@ -5,6 +5,8 @@ bench/tiers.o: \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
   src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs \
   bench/tiers.hs
 eg/test-text: \
   eg/test-text.hs \
@@ -13,35 +15,49 @@ eg/test-text.o: \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
   src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs \
   eg/test-text.hs
 mk/All.o: \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
   src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs \
   mk/All.hs
 mk/Toplibs.o: \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
   src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs \
   mk/Toplibs.hs
 Setup.o: \
   Setup.hs
 Setup: \
   Setup.hs \
   mk/toplibs
+src/Test/LeanCheck/Instances/ByteString.o: \
+  src/Test/LeanCheck/Instances/ByteString.hs
+src/Test/LeanCheck/Instances/Containers.o: \
+  src/Test/LeanCheck/Instances/Containers.hs
 src/Test/LeanCheck/Instances/Natural.o: \
   src/Test/LeanCheck/Instances/Natural.hs
 src/Test/LeanCheck/Instances.o: \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
-  src/Test/LeanCheck/Instances/Natural.hs
+  src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs
 src/Test/LeanCheck/Instances/Text.o: \
   src/Test/LeanCheck/Instances/Text.hs
 test/main.o: \
   test/main.hs \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
-  src/Test/LeanCheck/Instances/Natural.hs
+  src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs
 test/main: \
   test/main.hs \
   mk/toplibs
@@ -49,7 +65,9 @@ test/text.o: \
   test/text.hs \
   src/Test/LeanCheck/Instances/Text.hs \
   src/Test/LeanCheck/Instances.hs \
-  src/Test/LeanCheck/Instances/Natural.hs
+  src/Test/LeanCheck/Instances/Natural.hs \
+  src/Test/LeanCheck/Instances/Containers.hs \
+  src/Test/LeanCheck/Instances/ByteString.hs
 test/text: \
   test/text.hs \
   mk/toplibs
--- src/Test/LeanCheck/Instances/Containers.hs
+++ src/Test/LeanCheck/Instances/Containers.hs
@@ -4,10 +4,6 @@ import Test.LeanCheck
 
 import Data.Set (Set)
 import qualified Data.Set as Set
-import qualified Data.Set.Lazy as Lazy
 
 instance (Ord a, Listable a) => Listable (Set a) where
   tiers = setCons Set.fromList
-
-instance (Ord a, Listable a) => Listable (Lazy.Set a) where
-  tiers = setCons Lazy.fromList
