49c64c1237d3ae31f495254e930d45368b5d943b
Rudy Matela <rudy@matela.com.br>
2018-12-08 16:42:57 -0300

    Add Listable Lazy.Text instance

--- src/Test/LeanCheck/Instances/Text.hs
+++ src/Test/LeanCheck/Instances/Text.hs
@@ -1,7 +1,11 @@
 module Test.LeanCheck.Instances.Text () where
 
 import Data.Text
+import qualified Data.Text.Lazy as Lazy
 import Test.LeanCheck
 
 instance Listable Text where
   tiers = cons1 pack
+
+instance Listable Lazy.Text where
+  tiers = cons1 Lazy.pack
