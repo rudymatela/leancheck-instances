38396efd72d711f0bca920922d0ee958f7b3872f
Rudy Matela <rudy@matela.com.br>
2018-12-08 23:53:35 -0300

    Add Haddock documentation

--- src/Test/LeanCheck/Instances.hs
+++ src/Test/LeanCheck/Instances.hs
@@ -1,6 +1,32 @@
+-- |
+-- Module      : Test.LeanCheck
+-- Copyright   : (c) 2018 Rudy Matela
+-- License     : 3-Clause BSD  (see the file LICENSE)
+-- Maintainer  : Rudy Matela <rudy@matela.com.br>
+--
+-- This module provides "Test.LeanCheck" 'Listable' instances for types
+-- provided by the Haskell Platform.  More specifically, for types of the
+-- following packages:
+--
+-- * <https://hackage.haskell.org/package/bytestring bytestring>
+-- * <https://hackage.haskell.org/package/containers containers>
+-- * <https://hackage.haskell.org/package/nats       nats>
+-- * <https://hackage.haskell.org/package/text       text>
+--
+-- These instances are not defined in the
+-- <https://hackage.haskell.org/package/leancheck main leancheck package>
+-- to not introduce needless dependencies to LeanCheck.
+--
+-- These instances are defined in the following submodules:
+--
+-- * "Test.LeanCheck.Instances.Text"
+-- * "Test.LeanCheck.Instances.Natural"
+-- * "Test.LeanCheck.Instances.ByteString"
+-- * "Test.LeanCheck.Instances.Containers"
 module Test.LeanCheck.Instances () where
 
-import Test.LeanCheck.Instances.Text
-import Test.LeanCheck.Instances.Natural
-import Test.LeanCheck.Instances.ByteString
-import Test.LeanCheck.Instances.Containers
+import Test.LeanCheck (Listable) -- for Haddock
+import Test.LeanCheck.Instances.Text ()
+import Test.LeanCheck.Instances.Natural ()
+import Test.LeanCheck.Instances.ByteString ()
+import Test.LeanCheck.Instances.Containers ()
