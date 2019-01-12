dd7d94efa1b90b70c779944b62e1767e57b687a9
Rudy Matela <rudy@matela.com.br>
2019-01-12 15:55:20 -0300

    Add WithInstances module

---
+++ src/Test/LeanCheck/WithInstances.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : Test.LeanCheck.WithInstances
+-- Copyright   : (c) 2018 Rudy Matela
+-- License     : 3-Clause BSD  (see the file LICENSE)
+-- Maintainer  : Rudy Matela <rudy@matela.com.br>
+--
+-- "Test.LeanCheck" with additional 'Listable' instances.
+--
+-- Importhing this is the same as:
+--
+-- > import Test.LeanCheck
+-- > import Test.LeanCheck.Instances
+module Test.LeanCheck.WithInstances (module Test.LeanCheck, Listable) where
+
+import Test.LeanCheck
+import Test.LeanCheck.Instances
